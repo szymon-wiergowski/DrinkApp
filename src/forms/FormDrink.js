386b5869d31cda7c34084289978541e5fe964281
Szymon Wiergowski <s.wiergowski@gmail.com>
2019-12-29 20:47:18 +0100

    add elements of form

--- src/forms/FormDrink.js
+++ src/forms/FormDrink.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const strength = [
+  {
+    value: 'Mocny',
+    label: 'Mocny (pow. 15%)',
+  },
+  {
+    value: 'Słaby',
+    label: 'Słaby (od 0,5% do 15%)',
+  },
+  {
+    value: 'Bezalkoholowy',
+    label: 'Bezalkoholowy',
+  },
+];
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -11,141 +27,56 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+
 export function FormPropsTextFields() {
   const classes = useStyles();
+  const [strengthen, setPowercy] = React.useState('Bez alkoholu');
+
+  const handleChange = event => {
+    setPowercy(event.target.value);
+  };
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
-      <div>
-        <TextField required id="standard-required" label="Required" defaultValue="Hello World" />
-        <TextField disabled id="standard-disabled" label="Disabled" defaultValue="Hello World" />
-        <TextField
-          id="standard-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-        />
-        <TextField
-          id="standard-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-        />
-        <TextField
-          id="standard-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField id="standard-search" label="Search field" type="search" />
-        <TextField
-          id="standard-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-        />
-      </div>
-      <div>
-        <TextField
-          required
-          id="filled-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          disabled
-          id="filled-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          id="filled-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="filled"
-        />
-        <TextField
-          id="filled-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-          variant="filled"
-        />
-        <TextField
-          id="filled-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="filled"
-        />
-        <TextField id="filled-search" label="Search field" type="search" variant="filled" />
-        <TextField
-          id="filled-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="filled"
-        />
-      </div>
-      <div>
+      <div style={{ paddingTop: '6rem', paddingLeft: '1rem' }}>
         <TextField
           required
           id="outlined-required"
-          label="Required"
-          defaultValue="Hello World"
+          label="Nazwa"
+          defaultValue="..."
           variant="outlined"
         />
         <TextField
-          disabled
-          id="outlined-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
+          id="outlined-multiline-static"
+          label="Składniki"
+          multiline
+          rows="4"
+          defaultValue="Składniki potrzebna do przygotowania drinka"
           variant="outlined"
         />
         <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
+          id="outlined-multiline-static"
+          label="Opis"
+          multiline
+          rows="4"
+          defaultValue="Sposób przygotowania drinka"
           variant="outlined"
         />
         <TextField
-          id="outlined-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
+          id="outlined-select-currency"
+          select
+          label="Moc"
+          value={strengthen}
+          onChange={handleChange}
+          helperText="Wybierz moc drinka"
           variant="outlined"
-        />
-        <TextField
-          id="outlined-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-        />
-        <TextField id="outlined-search" label="Search field" type="search" variant="outlined" />
-        <TextField
-          id="outlined-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="outlined"
-        />
+        >
+          {strength.map(option => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
       </div>
     </form>
   );
