65bea4165b264026ce796c1379afb7f8e28ce8c5
Natalia <nnowak91@gmail.com>
2020-01-10 21:24:26 +0100

    drink_power_added

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class Drinks extends React.Component {
         <div>
 
           {
-            this.state.drinks.map(drink => <Drink key={drink.id} name={drink.name} recipe={drink.recipe} />)
+            this.state.drinks.map(drink => <Drink key={drink.id} name={drink.name} recipe={drink.power} />)
           }
         </div>
 
