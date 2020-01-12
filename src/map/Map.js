7b11b83fa7bf1eb127b1be2cc58a86c71dd7b747
Szymon Wiergowski <s.wiergowski@gmail.com>
2020-01-12 18:51:39 +0100

    FZ12JF-47_delate empty import

--- src/map/Map.js
+++ src/map/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
+import { Map, GoogleApiWrapper } from "google-maps-react";
 
 export class MapContainer extends Component {
   constructor(props) {
