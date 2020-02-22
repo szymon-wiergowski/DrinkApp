c9aa88474eb5fadacce53b36b612f426da0df691
Bartosz Walasek <b.wal1990@gmail.com>
2020-02-22 10:04:10 +0100

    change width in search panel

--- src/drink-filter-components/DrinkSearchField.js
+++ src/drink-filter-components/DrinkSearchField.js
@@ -12,7 +12,7 @@ export function DrinkSearchField(props) {
           <LocalBarRoundedIcon />
         </Grid>
         <Grid item>
-          <TextField value={props.valueSearchField} onChange={props.onChangeText} id="input-with-icon-grid" color="secondary" label={<em>Drink:</em>} />
+          <TextField value={props.valueSearchField} onChange={props.onChangeText} id="input-with-icon-grid" color="secondary" label={<em>Drink:</em>} style={{ width: 270 }}/>
         </Grid>
       </Grid>
     </div>
