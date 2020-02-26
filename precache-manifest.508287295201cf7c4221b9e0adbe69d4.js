f3fc177f412169a6d412125458a910cafcc95919
Szymon Wiergowski <s.wiergowski@gmail.com>
2020-02-26 20:32:20 +0100

    Updates

---
+++ precache-manifest.508287295201cf7c4221b9e0adbe69d4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd23389c40769c8a422c9af6041c4b50",
+    "url": "/drinkapp/index.html"
+  },
+  {
+    "revision": "79f08914bd0ae0f3a612",
+    "url": "/drinkapp/static/css/main.b3368cf4.chunk.css"
+  },
+  {
+    "revision": "9004d096de892b38ad97",
+    "url": "/drinkapp/static/js/2.ecf5f80f.chunk.js"
+  },
+  {
+    "revision": "29cd909fd605c5e2d99ff1367c319d8f",
+    "url": "/drinkapp/static/js/2.ecf5f80f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79f08914bd0ae0f3a612",
+    "url": "/drinkapp/static/js/main.2b178a12.chunk.js"
+  },
+  {
+    "revision": "eb3917aaba07ad2bbe3c",
+    "url": "/drinkapp/static/js/runtime-main.b685d4ef.js"
+  },
+  {
+    "revision": "434fa62203a17e4e3079f2e2992309a0",
+    "url": "/drinkapp/static/media/alkomat_safety.434fa622.png"
+  },
+  {
+    "revision": "a6ccc8f1cc7c38e48e07f53f42e4e215",
+    "url": "/drinkapp/static/media/drink.a6ccc8f1.png"
+  },
+  {
+    "revision": "5bad8da81bc92c87fd011ebe2bf16a4b",
+    "url": "/drinkapp/static/media/image_dashboard.5bad8da8.png"
+  },
+  {
+    "revision": "08070e8424101e6392db68452a5aea67",
+    "url": "/drinkapp/static/media/image_dashboard_small.08070e84.png"
+  },
+  {
+    "revision": "2aea4b73e5fd353f1bf27ea36359613e",
+    "url": "/drinkapp/static/media/map_shop.2aea4b73.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/drink-app/precache-manifest.9ab48e2bda76aef61ba62f4dfcd0c473.js"
+  "/drinkapp/precache-manifest.508287295201cf7c4221b9e0adbe69d4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/drink-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/drinkapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
